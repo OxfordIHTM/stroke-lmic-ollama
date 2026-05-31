468cd48426fdc99368ad3125b6b3a44fa2c80fb7
Ernest Guevarra <ernest@guevarra.io>
2026-05-31 16:19:09 +0100

    re-factor screening context and screening prompts

--- R/llm_screening_type.R
+++ R/llm_screening_type.R
@@ -8,14 +8,27 @@ llm_create_screening_type <- function() {
     population = ellmer::type_boolean(
       description = "Is the study about a population of adults?"
     ),
+    age_group = ellmer::type_string(
+      description = "Age, age group, age term, or age description",
+      required = FALSE
+    ),
     geography = ellmer::type_boolean(
       description = "Is the study conducted in a country classified as low-middle income or low income based on World Bank income classification?"
+    ),
+    geography_name = ellmer::type_string(
+      description = "Name of country", required = FALSE
     ),
     publication_type = ellmer::type_boolean(
       description = "Is the study a primary study with a research design such as a cohort study, case-control study, cross-sectional study, or interventional studies"
     ),
+    publication_type_name = ellmer::type_string(
+      description = "Type of publication", required = FALSE
+    ),
     topic = ellmer::type_boolean(
       description = "Is the study regarding stroke burden (such as prevalence, incidence, disability-adjusted life years, quality-adjusted life years, disability, death, hospitalisation) OR types of stroke (such as ischaemic stroke, haemorrhagic stroke) OR risk factors for stroke OR interventions for stroke."
+    ),
+    topic_name = ellmer::type_string(
+      description = "Study topic about stroke", required = FALSE
     )
   )
 }
