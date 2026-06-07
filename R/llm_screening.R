6601da1aac4fe0f741c06b2abc27caec42652736
Ernest Guevarra <ernest@guevarra.io>
2026-06-07 09:13:02 +0100

    refactor chat_structured function

--- R/llm_screening.R
+++ R/llm_screening.R
@@ -16,8 +16,6 @@ llm_screen_articles <- function(reviewer, query, type) {
 #' 
 
 llm_parallel_screen_articles <- function(reviewer, query, type) {
-  reviewer <- reviewer$set_turns(list())
-
   ellmer::parallel_chat_structured(
     chat = reviewer, prompts = query,
     on_error = "continue", type = type
