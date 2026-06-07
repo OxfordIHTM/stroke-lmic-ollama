16d442da4303265b09a6ebc08638566bcf2db935
Ernest Guevarra <ernest@guevarra.io>
2026-06-07 19:03:11 +0100

    edit prompt for middle income and low income countries

--- R/get_country_list.R
+++ R/get_country_list.R
@@ -6,11 +6,26 @@
 #' 
 
 get_country_list <- function(wb_df) {
-  wb_df |>
+  x <- wb_df |>
     dplyr::filter(income_group != "High income") |>
-    dplyr::pull(economy) |>
-    (\(x) paste0("  - ", x))() |>
-    cat(sep = "\n")
+    dplyr::pull(economy)
+
+  x[x == "Congo, Dem. Rep."]          <- "Democratic Republic of the Congo"
+  x[x == "Congo, Rep."]               <- "Congo-Brazzaville"
+  x[x == "Egypt, Arab Rep."]          <- "Egypt"
+  x[x == "Gambia, The"]               <- "Gambia"
+  x[x == "Iran, Islamic Rep."]        <- "Iran"
+  x[x == "Korea, Dem. People's Rep."] <- "North Korea"
+  x[x == "Kyrgyz Republic"]           <- "Kyrgyzstan"
+  x[x == "Lao PDR"]                   <- "Laos"
+  x[x == "Micronesia, Fed. Sts."]     <- "Micronesia"
+  x[x == "Somalia, Fed. Rep."]        <- "Somalia"
+  x[x == "Syrian Arab Republic"]      <- "Syria"
+  x[x == "Yemen, Rep."]               <- "Yemen"
+  
+  x |>
+    (\(x) paste0("\n- ", x))() |>
+    paste(collapse = "  ")
 }
 
 
